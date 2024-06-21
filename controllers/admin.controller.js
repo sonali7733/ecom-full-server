db2cfe27bb78968fec161c3fccec10f0492a8f50
SONALI NALAWADE <sonalinalawade7733@gmail.com>
2024-06-21 14:45:29 +0530

    initial setup new

---
+++ controllers/admin.controller.js
@@ -0,0 +1,91 @@
+const asyncHandler = require("express-async-handler")
+const Product = require("../models/Product")
+const Order = require("../models/Order")
+const { upload } = require("../utils/upload")
+const cloudinary = require("cloudinary").v2
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+})
+
+// product
+
+exports.getAllProducts = asyncHandler(async (req, res) => {
+    const result = await Product.find()
+    res.json({ message: "Product Fetch Success", result })
+})
+exports.addProduct = asyncHandler(async (req, res) => {
+    upload(req, res, async (err) => {
+        if (err) {
+            console.log(err)
+            return res.status(400).json({ message: "upload error" })
+        }
+        const { secure_url } = await cloudinary.uploader.upload(req.file.path)
+        // console.log(req.file.path) // s3 - company
+        const result = await Product.create({ ...req.body, images: secure_url })
+        res.json({ message: "Product Add Success", result })
+    })
+})
+exports.updateProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndUpdate(id)
+    res.json({ message: "Product Update Success" })
+})
+exports.deleteProduct = asyncHandler(async (req, res) => {
+    const result = await Product.findById(req.params.id)
+    const str = result.images.split("/")
+    const img = str[str.length - 1].split(".")[0]
+    await cloudinary.uploader.destroy(img)
+    await Product.findByIdAndDelete(req.params.id)
+    res.json({ message: "Product Delete Success" })
+})
+exports.deactivateProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndUpdate(id)
+    res.json({ message: "Product Deactivate Success" })
+})
+exports.activateProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndUpdate(id)
+    res.json({ message: "Product Activate Success" })
+})
+exports.getProductDetails = asyncHandler(async (req, res) => {
+    const result = await Product.find()
+    res.json({ message: "Product Detail Fetch Success", result })
+})
+
+// order
+
+exports.getAllOrders = asyncHandler(async (req, res) => {
+    const result = await Order.find()
+    res.json({ message: "Order Fetch Success" })
+})
+exports.getOrderDetails = asyncHandler(async (req, res) => {
+    res.json({ message: "Order Delete Fetch Success" })
+})
+exports.cancelOrder = asyncHandler(async (req, res) => {
+    res.json({ message: "Order Cancel Success" })
+})
+exports.updateOrderStatus = asyncHandler(async (req, res) => {
+    res.json({ message: "Order Status Update Success" })
+})
+
+// user
+
+exports.getAllUsers = asyncHandler(async (req, res) => {
+    res.json({ message: "Users Fetch Success" })
+})
+exports.getUserDetails = asyncHandler(async (req, res) => {
+    res.json({ message: "Users Detail Fetch Success" })
+})
+exports.blockUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Users Block Success" })
+})
+exports.unblockUser = asyncHandler(async (req, res) => {
+    res.json({ message: "Users Un-Block Success" })
+})
+exports.getUserOrders = asyncHandler(async (req, res) => {
+    res.json({ message: "Users Order Fetch Success" })
+})
---
+++ index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+require("dotenv").config()
+
+mongoose.connect(process.env.MONGO_URL)
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}))
+app.use(express.json())
+
+app.use("/api/admin", require("./routes/admin.routes"))
+app.use("/api/auth", require("./routes/auth.routes"))
+
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "Resource Not Found" })
+})
+app.use((err, req, res, next) => {
+    console.log(err)
+    res.status(500).json({ message: "Server Error", err: err.message })
+})
+
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONNECTED")
+    app.listen(process.env.PORT, console.log("server running"))
+})
